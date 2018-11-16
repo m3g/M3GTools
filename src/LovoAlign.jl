7e182eb7a61ef91f8f3ecb0d5b6f01fba0e8d99b
Leandro Martinez <leandromartinez98@gmail.com>
2018-11-16 11:46:07 -0200

    initial commit of nonseq function

--- src/LovoAlign.jl
+++ src/LovoAlign.jl
@@ -6,39 +6,43 @@ module LovoAlign
                   lovoalign_exec="lovoalign")
 
     vmd_input = open("./LovoAlign_VMDINPUT_TMP.VMD","w")
-    write("""
-          mol new $pdb1
-          set all [ atomselect top all ]
-          $all set beta 0
-          set sel [ atomselect top "$sel1" ]
-          $sel set beta 1
-          $all writepdb ./LovoAlign_sel1.pdb
-          $all delete
-          $sel delete
-          
-          mol new $pdb2
-          set all [ atomselect top all ]
-          $all set beta 0
-          set sel [ atomselect top "$sel2" ]
-          $sel set beta 1
-          $all writepdb ./LovoAlign_sel2.pdb
-          $all delete
-          $sel delete
-          
-          exit
-          """)
+    write(vmd_input,"""
+                    mol new \"$pdb1\"
+                    set all [ atomselect top all ]
+                    \$all set beta 0
+                    set sel [ atomselect top \"$sel1\" ]
+                    \$sel set beta 1
+                    \$all writepdb ./LovoAlign_sel1.pdb
+                    \$all delete
+                    \$sel delete
+                    
+                    mol new \"$pdb2\"
+                    set all [ atomselect top all ]
+                    \$all set beta 0
+                    set sel [ atomselect top \"$sel2\" ]
+                    \$sel set beta 1
+                    \$all writepdb ./LovoAlign_sel2.pdb
+                    \$all delete
+                    \$sel delete
+                    
+                    exit
+                    """)
     close(vmd_input)
 
     # Run VMD
-    run(`$vmd_exec -dispdev text -e ./LovoAlign_VMDINPUT_TMP.VMD`)
+    vmd_output = read(`$vmd_exec -dispdev text -e ./LovoAlign_VMDINPUT_TMP.VMD`,String)
 
     # Run LovoAlign
-    run(`$lovoalign_exec -p1 LovoAlign_sel1.pdb -p2 LovoAlign_sel2.pdb -g 0. -beta1 -beta2 -m 3 -all -nglobal 10 -o $output`)
+    lovoalign_output=read(
+     `$lovoalign_exec -p1 LovoAlign_sel1.pdb -p2 LovoAlign_sel2.pdb -g 0. -beta1 -beta2 -m 3 -all -nglobal 50 -dtri 10. -o $output`,
+     String)
 
     # Remove temporary files
     run(`\rm -f ./LovoAlign_VMDINPUT_TMP.VMD`)
     run(`\rm -f ./LovoAlign_sel1.pdb`)
     run(`\rm -f ./LovoAlign_sel2.pdb`)
+
+    return "Wrote $output file with $pdb1 aligned to $pdb2"
 
   end
 
