9ba04c8236c09a1d9d1a835fe5511a993b201bf7
Leandro Martinez <leandro@iqm.unicamp.br>
2018-11-19 13:30:08 -0200

    moved cm to other file

--- src/Namd.jl
+++ src/Namd.jl
@@ -251,26 +251,7 @@ module Namd
 
   end
 
-  #
-  # This function computes the center of mass of a selection
-  #
-  
-  function cm(selection,mass,x,y,z)
-  
-    cm = [ 0., 0., 0. ]
-    totmass = 0.
-    for i in 1:length(selection)
-      cm[1] = cm[1] + mass[selection[i]]*x[selection[i]] 
-      cm[2] = cm[2] + mass[selection[i]]*y[selection[i]] 
-      cm[3] = cm[3] + mass[selection[i]]*z[selection[i]] 
-      totmass = totmass + mass[selection[i]]
-    end
-    cm[1] = cm[1] / totmass
-    cm[2] = cm[2] / totmass
-    cm[3] = cm[3] / totmass
-  
-    return cm
-  end
+  include("./Cm.jl")
 
 end
 
