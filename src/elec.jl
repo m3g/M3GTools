376d90968449754bf7971d2643e0e2ed8f57304c
Leandro Martinez <leandro@iqm.unicamp.br>
2018-12-14 11:11:07 -0200

    fixed n2

--- src/elec.jl
+++ src/elec.jl
@@ -13,7 +13,7 @@ function elec(sel1,sel2,charge,sides,x,y,z)
   for i in 1:n1
     center = [ x[sel1[i]], y[sel1[i]], z[sel1[i]] ]
     wrap!(sides,x,y,z,center=center,sel=sel2)
-    for j in 1:n
+    for j in 1:n2
       d = sqrt((x[sel1[i]] - x[sel2[j]])^2 + (y[sel1[i]] - y[sel2[j]])^2 + (z[sel1[i]] - z[sel2[j]])^2)
       elec = elec + qpair(d,q1,q2)
     end
