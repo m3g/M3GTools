b307d75ae598ee252754240e07c88f6b718a107b
Leandro Martinez <leandro@iqm.unicamp.br>
2019-05-29 14:51:11 -0300

    added new select method to read directly the topology file

--- src/select.jl
+++ src/select.jl
@@ -1,15 +1,16 @@
-
 # 
 # Select atoms using vmd selection syntax, with vmd in background
 #
 
-function select(simulation :: Simulation, selection :: String)
+# Method to return the selection from the topology file, by calling VMD in the background.
+
+function select( topology :: String, selection :: String; vmd="vmd" )
 
   local index_list :: String 
   local readnext :: Bool = false
 
   vmd_input = Base.open("./VMDINPUT_TMP.VMD","w")
-  Base.write(vmd_input,"mol new \"$(simulation.psf)\" \n")
+  Base.write(vmd_input,"mol new \"$topology\" \n")
   Base.write(vmd_input,"set sel [ atomselect top \"$selection\" ] \n")
   Base.write(vmd_input,"puts \"INDEXLIST\" \n")
   Base.write(vmd_input,"set indexes [ \$sel get index ] \n")
@@ -17,7 +18,7 @@ function select(simulation :: Simulation, selection :: String)
   Base.write(vmd_input,"exit \n")
   Base.close(vmd_input)
 
-  vmd_output = read(`$(simulation.vmd) -dispdev text -e ./VMDINPUT_TMP.VMD`, String)
+  vmd_output = read(`$vmd -dispdev text -e ./VMDINPUT_TMP.VMD`, String)
 
   for line in split(vmd_output,"\n")
     if readnext
@@ -43,6 +44,10 @@ function select(simulation :: Simulation, selection :: String)
   return selection_indexes
 
 end
+
+# Method to return the selection if what was provided was the Simulation object. 
+
+select(simulation :: Simulation, selection :: String) = select( "$(simulation.psf)", selection, vmd="$(simulation.vmd)" )
 
 #
 # select atoms using VMD selection syntax
