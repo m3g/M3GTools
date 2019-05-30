0a2b7c5a4b61bac33c2c29b714776aa9a2b86998
Leandro Martinez <leandro@iqm.unicamp.br>
2019-05-30 11:54:56 -0300

    updated fig

--- examples/gmd_solute/gmdfiles/figs.jl
+++ examples/gmd_solute/gmdfiles/figs.jl
@@ -11,6 +11,25 @@ using Namd
 using DelimitedFiles
 using LaTeXStrings
 
+function smooth(data)
+  # smooth data
+  n = length(data)
+  y = copy(data)
+  nav = 10
+  for i in nav+1:n-nav
+    av = 0.
+    for j in i-nav:i+nav
+      av = av + data[j]
+    end
+    y[i] = av / (2*nav)
+  end
+  for i in 1:nav
+    y[i] = data[i]
+    y[n-i] = data[n-i]
+  end
+  return y
+end 
+
 println(" Loading Plots... ")
 using Plots
 ENV["GKSwstype"]="nul" # This supresses the need of a display while plotting
@@ -26,16 +45,26 @@ gmd_solute = readdlm("./gmd-GMD_ATOM_SOLUTE_CONTRIB.dat",comments=true,comment_c
 d = gmd_solute[:,1]
 gmd_total = gmd_solute[:,2]
 
+gmd_total = smooth(gmd_total)
+
 # Define the solute selection used for computation of the GMD here:
-gmd_backbone = Namd.gmdget(topology,gmd_solute,data="protein",get="protein and (backbone or name HN)")
-gmd_aliphatic = Namd.gmdget(topology,gmd_solute,data="protein",get="protein and aliphatic")
-gmd_charged = Namd.gmdget(topology,gmd_solute,data="protein",get="protein and charged")
+gmd_backbone = Namd.gmdget(topology,gmd_solute,data="protein",get="protein and (not sidechain)")
+gmd_aliphatic = Namd.gmdget(topology,gmd_solute,data="protein",get="protein and sidechain and aliphatic")
+gmd_aromatic = Namd.gmdget(topology,gmd_solute,data="protein",get="protein and sidechain and aromatic")
+gmd_polar = Namd.gmdget(topology,gmd_solute,data="protein",get="protein and sidechain and polar")
+
+gmd_backbone = smooth(gmd_backbone)
+gmd_aliphatic = smooth(gmd_aliphatic)
+gmd_aromatic = smooth(gmd_aromatic)
+gmd_polar = smooth(gmd_polar)
 
 # Plot the results
-plot(d,gmd_total,label="Total",xlim=[0,8],xlabel=L"\textrm{Distance / \AA}",ylabel=L"\textrm{MDDF}")
-plot!(d,gmd_backbone,label="Backbone")
-plot!(d,gmd_aliphatic,label="Aliphatic")
-plot!(d,gmd_charged,label="Charged")
+plot(d,gmd_total,label="Total",xlim=[0,8],xlabel=L"\textrm{Distance / \AA}",ylabel=L"\textrm{MDDF}",linewidth=2)
+plot!(d,gmd_backbone,label="Backbone",linewidth=2)
+plot!(d,gmd_aliphatic,label="Aliphatic",linewidth=2)
+plot!(d,gmd_aromatic,label="Aromatic",linewidth=2)
+plot!(d,gmd_polar,label="Polar",linewidth=2)
+plot!(fontsize=16)
 savefig("./gmd_total.png")
 println(" Created plot: ./gmd_total.png")
 
