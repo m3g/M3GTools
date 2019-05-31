bec244ac952fe5dce6c119dfe68a56cc112873fc
Leandro Martinez <leandro@iqm.unicamp.br>
2019-05-31 11:48:14 -0300

    updated version file to 19.151.2.3

--- src/release.jl
+++ src/release.jl
@@ -15,8 +15,8 @@ run(`git commit -m "updated version file to $version"`)
 run(`git tag -a $version -m "Release $version"`)
 run(`git push origin master tag $version`)
 
-range = read(`git tag | tail -n 2 | xargs | sed 's! !...!'`)
-tagdiff = read(`git log --pretty=oneline $range | awk '{$1=""; print "-"$0}'`)
+range = read(`git tag \| tail -n 2 \| xargs \| sed 's! !...!'`)
+tagdiff = read(`git log --pretty=oneline $range \| awk '{$1=""; print "-"$0}'`)
 
 println("----------------------")
 println("CHANGE LOG:")
