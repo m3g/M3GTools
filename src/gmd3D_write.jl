7240907c154593366ad892b5274e8dc17066edff
Leandro Martinez <leandro@iqm.unicamp.br>
2019-12-05 13:25:54 -0300

    fixed calling of gmd3D_write

--- src/gmd3D_write.jl
+++ src/gmd3D_write.jl
@@ -2,30 +2,30 @@
 # Function that writes the gmd3D to a pdb file
 #
 
-# If called already with the gmd3D computed
+# If called already with the grid of the gmd3D computed
 
 function gmd3D_write(mysim :: Simulation, grid :: Vector{GMD3DGrid}, output :: String)
 
-  n = length(gmd3D)
-  minrho = gmd3D[1].rho
-  maxrho = gmd3D[1].rho
+  n = length(grid)
+  minrho = grid[1].rho
+  maxrho = grid[1].rho
   for i in 2:n
-    minrho = min(minrho,gmd3D[i].rho)
-    maxrho = max(maxrho,gmd3D[i].rho)
+    minrho = min(minrho,grid[i].rho)
+    maxrho = max(maxrho,grid[i].rho)
   end
 
   file = open(output,"w")
   for i in 1:n
-    iat = gmd3D[i].atom
+    iat = grid[i].atom
     name = mysim.atoms[iat].name
     resname = mysim.atoms[iat].resname
     chain = mysim.atoms[iat].chain
     resnum = mysim.atoms[iat].resid
-    x = gmd3D[i].x[1]
-    y = gmd3D[i].x[2]
-    z = gmd3D[i].x[3]
-    b = 99.9*(gmd3D[i].rho - minrho)/(maxrho-minrho)
-    occup = gmd3D[i].dmin
+    x = grid[i].x[1]
+    y = grid[i].x[2]
+    z = grid[i].x[3]
+    b = 99.9*(grid[i].rho - minrho)/(maxrho-minrho)
+    occup = grid[i].dmin
     model = 0
     atom = PDBTools.Atom(i,name,resname,chain,resnum,x,y,z,b,occup,model)
     println(file,PDBTools.write_atom(atom))
