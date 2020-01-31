735936b5f9f68e7f4eca61b37cb257ea16201bf7
Leandro Martinez <leandro@iqm.unicamp.br>
2020-01-31 15:12:06 -0300

    fixed minrho

--- src/gmd3D_write.jl
+++ src/gmd3D_write.jl
@@ -26,7 +26,7 @@ function gmd3D_write(mysim :: Simulation, grid :: Vector{GMD3DGrid}, output :: S
   println(file,"REMARK  Occupancy column contains distance to solute. ")
   println(file,"REMARK  B-factor column contains the scaled density, such that 99.9 is the maximum ")
   println(file,"REMARK  density obsered and 0. is the minimum density observed. The actual limits are: ")
-  println(file,"REMARK  Minimum density: $maxrho")
+  println(file,"REMARK  Minimum density: $minrho")
   println(file,"REMARK  Maximum density: $maxrho")
   for i in 1:n
     iat = grid[i].atom
